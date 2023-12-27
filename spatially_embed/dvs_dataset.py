e6313ae01800a681a4de84f76f85f4130c70c000
Jackn0 <36100042+Jackn0@users.noreply.github.com>
2023-12-27 17:18:11 +0800

    Update dvs_dataset.py

--- spatially_embed/dvs_dataset.py
+++ spatially_embed/dvs_dataset.py
@@ -18,8 +18,8 @@ def load_dvs128_gesture(distributed=False, T=10, data_dir = 'dvs128_gesture'):
     return train_set, test_set, train_sampler, test_sampler
 
 def load_n_mnist(distributed=False, T=10, data_dir = 'n_mnist'):
-    train_set = n_mnist.NMNIST(data_dir, train=True, data_type='frame', frames_number=T)
-    test_set = n_mnist.NMNIST(data_dir, train=False, data_type='frame', frames_number=T)
+    train_set = n_mnist.NMNIST(data_dir, train=True, data_type='frame', split_by='time', frames_number=T)
+    test_set = n_mnist.NMNIST(data_dir, train=False, data_type='frame', split_by='time', frames_number=T)
     if distributed:
         train_sampler = torch.utils.data.distributed.DistributedSampler(train_set)
         test_sampler = torch.utils.data.distributed.DistributedSampler(test_set)
@@ -29,6 +29,7 @@ def load_n_mnist(distributed=False, T=10, data_dir = 'n_mnist'):
     return train_set, test_set, train_sampler, test_sampler
 
 if __name__ == '__main__':
-    os.makedirs('n_mnist', exist_ok=True)
-    os.makedirs('n_mnist/download', exist_ok=True)
-    train_set, test_set, _, _ = load_n_mnist(distributed=False, T=10, data_dir = 'n_mnist')
+    # os.makedirs('n_mnist', exist_ok=True)
+    # os.makedirs('n_mnist/download', exist_ok=True)
+    train_set, test_set, _, _ = load_n_mnist(distributed=False, T=10, data_dir = '/data1/dsk/dataset/n_mnist')
+    print(train_set[0][0].shape)
