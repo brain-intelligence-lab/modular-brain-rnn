f3f46456492c3ce92687736a31c92d547291679a
Jackn0 <36100042+Jackn0@users.noreply.github.com>
2023-12-28 17:11:46 +0800

    Update node_distance.py

--- spatially_embed/node_distance.py
+++ spatially_embed/node_distance.py
@@ -3,7 +3,7 @@ import torch
 
 def sphere_distance(n, r):
     '''
-    创建一个球体，将N个点均匀分布到球面上，计算每个点之间的距离
+    创建一个球体，将N个点平均分布到球面上，计算每个点之间的距离
     :param n: int, 点的数量
     :param r: float, 球体半径
     :return D: torch.tensor, N*N, 每个点之间的距离
@@ -11,9 +11,9 @@ def sphere_distance(n, r):
     # 1. 创建一个球体
     center = np.array([0, 0, 0])  # 球体中心坐标
 
-    # 2. 将N个点平均分布到球体上
-    theta = np.linspace(0, 2 * np.pi, n)  # 极角范围
-    phi = np.arccos(1 - 2 * np.linspace(0, 1, n))  # 极化角范围
+    # 2. 将N个点平均分布到球面上
+    phi = np.arccos(1 - 2 * np.arange(n) / (n - 1))  # 仰角
+    theta = np.pi * (1 + 5 ** 0.5) * np.arange(n)  # 方位角
 
     x = center[0] + r * np.sin(phi) * np.cos(theta)  # x坐标
     y = center[1] + r * np.sin(phi) * np.sin(theta)  # y坐标
@@ -23,22 +23,23 @@ def sphere_distance(n, r):
 
     D = np.sqrt(np.sum((points[:, np.newaxis] - points) ** 2, axis=2))
 
-    return torch.from_numpy(D).type(torch.float)
+    return torch.from_numpy(D).type(torch.float), points
 
 def cube_distance(n):
     '''
-    创建一个立方体，将N个点均匀分布到立方体内，计算每个点之间的距离
+    创建一个立方体，将N个点平均分布到立方体内，计算每个点之间的距离
     :param n:  int, 点的数量
     :return:  D: torch.tensor, N*N, 每个点之间的距离
     '''
 
     # 1. 创建一个立方体
     # 计算立方体的边长，a, b, c 满足条件 a*b*c = N 且 a,b,c 接近 N^(1/3)，a,b,c为整数
-    a = int(np.round(n ** (1 / 3)))
-    b = int(np.round(n ** (1 / 3)))
-    c = int(n / (a * b))
+    a = int(np.ceil(n ** (1 / 3)))
+    b = int(np.ceil(n ** (1 / 3)))
+    c = int(np.ceil(n / (a * b)))
+    print(a,b,c)
 
-    # 2. 将N个点平均分布到立方体上
+    # 2. 将N个点平均分布到立方体内
     x = np.linspace(0, a - 1, a)
     y = np.linspace(0, b - 1, b)
     z = np.linspace(0, c - 1, c)
@@ -50,11 +51,14 @@ def cube_distance(n):
 
     points = np.column_stack((x, y, z))  # 合并坐标
 
+    points = points[0:n, :]
+
     D = np.sqrt(np.sum((points[:, np.newaxis] - points) ** 2, axis=2))
 
-    return torch.from_numpy(D).type(torch.float)
+
+    return torch.from_numpy(D).type(torch.float), points
 
 if __name__ == '__main__':
-    N = 100
-    D = cube_distance(N)
-    print(D)
+    N = 10
+    print(N**(1/3))
+    D, M = sphere_distance(N, 1)
